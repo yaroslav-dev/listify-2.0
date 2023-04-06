108fd3ef29125ecb87f28d7e0b78d67bc92e253b
Yaroslav Tuvarzhiiev <yaroslav.tuvarzhiiev@gmail.com>
2023-04-06 12:07:15 +0300

    manifest fix 3

--- vite.config.ts
+++ vite.config.ts
@@ -30,23 +30,23 @@ export default defineConfig({
         theme_color: '#ffffff',
         icons: [
           {
-            src: 'android-chrome-192x192.png',
+            src: 'img/icons/android-chrome-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'android-chrome-512x512.png',
+            src: 'img/icons/android-chrome-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           },
           {
-            src: 'android-chrome-512x512.png',
+            src: 'img/icons/android-chrome-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable'
           },
           {
-            src: 'apple-touch-icon.png',
+            src: 'img/icons/apple-touch-icon.png',
             type: 'image/png'
           },
           {
