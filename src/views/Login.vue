470ae8a8aef3d5e5744a65a7dc3504667a150eaa
Yaroslav Tuvarzhiiev <yaroslav.tuvarzhiiev@gmail.com>
2023-05-12 19:13:21 +0300

    redirect auth test 2

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -21,7 +21,7 @@ import { useAppStore } from '@/store/app';
 import { useUserStore } from '@/store/user';
 import { onBeforeUnmount, onBeforeMount, onMounted } from 'vue';
 import { useFirebaseAuth } from 'vuefire';
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider, signInWithPopup, signInWithRedirect, getRedirectResult } from "firebase/auth";
 import { useRouter } from 'vue-router';
 import { usePersist } from '@/store/persist';
 
@@ -54,16 +54,41 @@ onMounted(() => {
 
 
 const signInWithGoogle = async () => {
-  signInWithPopup(auth, provider)
-    .then(result => {
-      const credential = GoogleAuthProvider.credentialFromResult(result)
-      const token = credential?.accessToken
-      const user = result.user
-      userStore.setUser(user)
+  // signInWithPopup(auth, provider)
+  //   .then(result => {
+  //     const credential = GoogleAuthProvider.credentialFromResult(result)
+  //     const token = credential?.accessToken
+  //     const user = result.user
+  //     userStore.setUser(user)
+  //     persist.setPersist(res)
+  //     router.push({name: 'Home'})
+  //   }).then(() => {
+  //   }).catch(error => {
+  //     // Handle Errors here.
+  //     const errorCode = error.code;
+  //     const errorMessage = error.message;
+  //     // The email of the user's account used.
+  //     const email = error.customData.email;
+  //     // The AuthCredential type that was used.
+  //     const credential = GoogleAuthProvider.credentialFromError(error);
+  //   })
+  signInWithRedirect(auth, provider)
+}
+const getRes = () => {
+  getRedirectResult(auth)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access Google APIs.
+      const credential = GoogleAuthProvider.credentialFromResult(result!);
+      const token = credential?.accessToken;
+  
+      // The signed-in user info.
+      const user = result?.user;
+      // IdP data available using getAdditionalUserInfo(result)
+      // ...
+      userStore.setUser(user!)
       persist.setPersist(res)
       router.push({name: 'Home'})
-    }).then(() => {
-    }).catch(error => {
+    }).catch((error) => {
       // Handle Errors here.
       const errorCode = error.code;
       const errorMessage = error.message;
@@ -71,10 +96,12 @@ const signInWithGoogle = async () => {
       const email = error.customData.email;
       // The AuthCredential type that was used.
       const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
     })
 }
 
 onBeforeMount(() => {
+  getRes()
   store.hideNavBar(true)
 })
 onBeforeUnmount(() => {
