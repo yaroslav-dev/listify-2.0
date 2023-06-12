51916ea3f0d54a8b5d822792f004bdc93f306ed2
Yaroslav Tuvarzhiiev <yaroslav.tuvarzhiiev@gmail.com>
2023-06-12 11:55:54 +0300

    user store fix

--- src/store/user.ts
+++ src/store/user.ts
@@ -2,9 +2,7 @@ import { defineStore } from "pinia";
 import { ref } from "vue";
 
 export const useUserStore = defineStore('user', () => {
-  const user = ref({
-
-  })
+  const user = ref({})
 
   const setUser = (newUser: object) => {
     user.value = newUser
