eee481d26a0f8af6d05931f495960ac653ddd564
Fender420 <stoned.fender@gmail.com>
2023-07-17 09:57:55 +0300

    formatted

--- src/components/list/ListItem.vue
+++ src/components/list/ListItem.vue
@@ -1,14 +1,32 @@
 <template>
-  <v-card class="col-md-6 my-2 d-flex align-center" :color="listItem.color || '#f8f6ff'" :ripple="false" elevation="0" density="compact" @click.stop="editItem">
+  <v-card
+    class="col-md-6 my-2 d-flex align-center"
+    :color="listItem.color || '#f8f6ff'"
+    :ripple="false"
+    elevation="0"
+    density="compact"
+    @click.stop="editItem"
+  >
     <v-card-actions>
-      <v-checkbox v-model="listItem.completed" @click.stop :ripple="false" color="primary" density="compact"
-        hide-details></v-checkbox>
+      <v-checkbox
+        v-model="listItem.completed"
+        @click.stop
+        :ripple="false"
+        color="primary"
+        density="compact"
+        hide-details
+      ></v-checkbox>
     </v-card-actions>
     <v-card-text>
       {{ listItem.title }}
     </v-card-text>
     <v-card-actions>
-      <v-btn density="compact" icon="drag_indicator"></v-btn>
+      <v-btn
+        density="compact"
+        class="handle"
+        icon="drag_indicator"
+        @touchstart="vibro"
+      ></v-btn>
     </v-card-actions>
     <v-dialog transition="dialog-bottom-transition" v-model="dialog" fullscreen>
       <v-card>
@@ -20,64 +38,89 @@
         </v-toolbar>
         <v-form class="ma-4">
           <div class="text-h6">Title</div>
-          <v-text-field bg-color="#fff" color="primary" density="compact" v-model="tempTitle"></v-text-field>
+          <v-text-field
+            bg-color="#fff"
+            color="primary"
+            density="compact"
+            v-model="tempTitle"
+          ></v-text-field>
           <div class="text-h6">Background color</div>
-          <v-color-picker class="mx-auto" v-model="tempColor" hide-canvas hide-inputs hide-sliders show-swatches :swatches="swatches" elevation="0"></v-color-picker>
+          <v-color-picker
+            class="mx-auto"
+            v-model="tempColor"
+            hide-canvas
+            hide-inputs
+            hide-sliders
+            show-swatches
+            :swatches="swatches"
+            elevation="0"
+          ></v-color-picker>
           <div class="d-flex mt-4">
-            <v-btn prepend-icon="delete" class="mx-auto" color="error" variant="text" @click="removeItem">
+            <v-btn
+              prepend-icon="delete"
+              class="mx-auto"
+              color="error"
+              variant="text"
+              @click="removeItem"
+            >
               Delete item
             </v-btn>
           </div>
         </v-form>
-        <v-card-actions>
-        </v-card-actions>
+        <v-card-actions> </v-card-actions>
       </v-card>
     </v-dialog>
   </v-card>
 </template>
 <script lang="ts" setup>
-import { computed, ref } from 'vue';
+import { computed, ref } from "vue";
 
-const emit = defineEmits(['delete-item'])
+const vibro = () => {
+  setTimeout(() => {
+    window.navigator.vibrate(50);
+  }, 100);
+};
 
-const props = defineProps(['item'])
+const emit = defineEmits(["delete-item"]);
 
-const listItem = computed(() => props.item)
+const props = defineProps(["item"]);
 
-const tempTitle = ref('')
+const listItem = computed(() => props.item);
+
+const tempTitle = ref("");
 
 const swatches = [
-  ['#FFCDD2', '#BBDEFB', '#DCEDC8'],
-  ['#F8BBD0', '#B3E5FC', '#F0F4C3'],
-  ['#E1BEE7', '#B2EBF2', '#FFF9C4'],
-  ['#D1C4E9', '#B2DFDB', '#FFECB3'],
-  ['#C5CAE9', '#C8E6C9', '#FFE0B2'],
-]
+  ["#FFCDD2", "#BBDEFB", "#DCEDC8"],
+  ["#F8BBD0", "#B3E5FC", "#F0F4C3"],
+  ["#E1BEE7", "#B2EBF2", "#FFF9C4"],
+  ["#D1C4E9", "#B2DFDB", "#FFECB3"],
+  ["#C5CAE9", "#C8E6C9", "#FFE0B2"],
+];
 
-const tempColor = ref('')
+const tempColor = ref("");
 
-const dialog = ref(false)
+const dialog = ref(false);
 
 const discard = () => {
-  dialog.value = false
-  tempTitle.value = `${listItem.value.title}`
-  tempColor.value = `${listItem.value.color}`
-}
+  dialog.value = false;
+  tempTitle.value = `${listItem.value.title}`;
+  tempColor.value = `${listItem.value.color}`;
+};
 
 const saveItem = () => {
-  dialog.value = false
-  listItem.value.title = `${tempTitle.value}`
-  listItem.value.color = `${tempColor.value}`
-}
+  dialog.value = false;
+  listItem.value.title = `${tempTitle.value}`;
+  listItem.value.color = `${tempColor.value}`;
+};
 
 const editItem = () => {
-  tempTitle.value = `${listItem.value.title}`
-  tempColor.value = `${listItem.value.color}`
-  dialog.value = true
-}
+  tempTitle.value = `${listItem.value.title}`;
+  tempColor.value = `${listItem.value.color}`;
+  dialog.value = true;
+};
 
 const removeItem = () => {
-  emit('delete-item', listItem.value.id)
-  dialog.value = false
-}
+  emit("delete-item", listItem.value.id);
+  dialog.value = false;
+};
 </script>
--- src/views/List.vue
+++ src/views/List.vue
@@ -1,76 +1,136 @@
 <template>
   <v-container class="container d-flex flex-column justify-space-between">
     <v-responsive v-if="list">
-      <v-text-field :autofocus="focusIfNewTitle" onfocus="focusIfNewTitle ? this.select() : null" density="compact" bg-color="#fff" color="primary" v-model="listsStore.currentList.title" hide-details></v-text-field>
-      <transition-group>
-        <ListItem v-for="(item, index) in list.items" :item="item" :key="index" @delete-item="deleteItem" />
-      </transition-group>
+      <v-text-field
+        :autofocus="focusIfNewTitle"
+        density="compact"
+        bg-color="#fff"
+        color="primary"
+        v-model="listsStore.currentList.title"
+        clear-icon="close"
+        clearable
+        hide-details
+      ></v-text-field>
+      <draggable
+        class="dragArea list-group w-full"
+        :list="list.items"
+        handle=".handle"
+        v-bind="dragOptions"
+        @change="log"
+      >
+        <transition-group>
+          <ListItem
+            v-for="(item, index) in list.items"
+            :item="item"
+            :key="index"
+            @delete-item="deleteItem"
+          />
+        </transition-group>
+      </draggable>
     </v-responsive>
-    <v-app-bar class="pt-0 px-3 pb-3" flat color="transparent" location="bottom">
+    <v-app-bar
+      class="pt-0 px-3 pb-3"
+      flat
+      color="transparent"
+      location="bottom"
+    >
       <TextField class="text-field" @add-item="addItem" />
     </v-app-bar>
   </v-container>
 </template>
 <script lang="ts" setup>
-import { useAppStore } from '@/store/app';
-import { useListsStore } from '@/store/lists';
-import { onBeforeUnmount, onBeforeMount, onMounted, onUnmounted, ref, watch, computed } from 'vue';
-import { useRouter } from 'vue-router';
-import TextField from '@/components/list/TextField.vue';
-import ListItem from '@/components/list/ListItem.vue';
-import { useFirestore } from 'vuefire';
-import { collection, doc, setDoc } from 'firebase/firestore';
+import { useAppStore } from "@/store/app";
+import { useListsStore } from "@/store/lists";
+import {
+  onBeforeUnmount,
+  onBeforeMount,
+  onMounted,
+  onUnmounted,
+  ref,
+  watch,
+  computed,
+} from "vue";
+import { useRouter } from "vue-router";
+import TextField from "@/components/list/TextField.vue";
+import ListItem from "@/components/list/ListItem.vue";
+import { useFirestore } from "vuefire";
+import { collection, doc, setDoc } from "firebase/firestore";
+import { VueDraggableNext as draggable } from "vue-draggable-next";
 
-const itemColor = ref('#f8f6ff')
+const log = (event: any) => {
+  console.log(event);
+};
 
-const router = useRouter()
-const store = useAppStore()
-const listsStore = useListsStore()
+const dragOptions = computed(() => {
+  return {
+    animation: 200,
+    group: "description",
+    disabled: false,
+    ghostClass: "ghost",
+    delay: 100,
+  };
+});
 
-const isCurrentListExists = ref(Object.keys(listsStore.currentList).length)
+const itemColor = ref("#f8f6ff");
 
-const focusIfNewTitle = computed(() => {
-  return listsStore.currentList.title == 'New list'
-})
+const router = useRouter();
+const store = useAppStore();
+const listsStore = useListsStore();
+
+const isCurrentListExists = ref(Object.keys(listsStore.currentList).length);
 
 onBeforeMount(() => {
   if (!isCurrentListExists.value) {
-    router.push('/')
+    router.push("/");
   }
-})
+});
 
-const list = ref()
+const list = ref();
 
 onMounted(() => {
   if (isCurrentListExists.value) {
-    list.value = listsStore.currentList
+    list.value = listsStore.currentList;
   }
-})
+});
+
+const focusIfNewTitle = computed(() => {
+  return !list.value.items.length;
+});
 
 onBeforeUnmount(() => {
-  listsStore.currentList = {}
-})
+  listsStore.currentList = {};
+});
 
 onUnmounted(() => {
-  store.hideNavBar(false)
-})
+  store.hideNavBar(false);
+});
 
-const fire = useFirestore()
-const currentUser = JSON.parse(localStorage['currentUser'])
+const fire = useFirestore();
+const currentUser = JSON.parse(localStorage["currentUser"]);
 
-watch(list, () => {
-  if (isCurrentListExists.value) {
-    setDoc(doc(collection(doc(collection(fire, 'users'), currentUser.id), 'lists'), `${list.value.id}`), list.value)
-  }
-}, {deep: true})
+watch(
+  list,
+  () => {
+    if (isCurrentListExists.value) {
+      setDoc(
+        doc(
+          collection(doc(collection(fire, "users"), currentUser.id), "lists"),
+          `${list.value.id}`
+        ),
+        list.value
+      );
+    }
+  },
+  { deep: true }
+);
 
 const addItem = (title: string) => {
-  listsStore.addItem(title, itemColor.value)
-}
+  listsStore.addItem(title, itemColor.value);
+};
 
 const deleteItem = (id: number) => {
-  listsStore.deleteItem(id)
-}
+  listsStore.deleteItem(id);
+};
 </script>
 <style scoped>
 .container {
@@ -78,7 +138,7 @@ const deleteItem = (id: number) => {
 }
 .v-enter-active,
 .v-leave-active {
-  transition: opacity .3s ease;
+  transition: opacity 0.3s ease;
 }
 
 .v-enter-from,
